62716dc37640c40c6021e43e30e4b808916fa196
jumpman <jumpman23x@protonmail.com>
2022-09-02 15:48:13 -0400

    initial commit

---
+++ src/components/Menu/config/footerConfig.ts
@@ -0,0 +1,74 @@
+import { FooterLinkType } from '@pancakeswap/uikit'
+import { ContextApi } from '@pancakeswap/localization'
+
+export const footerLinks: (t: ContextApi['t']) => FooterLinkType[] = (t) => [
+  {
+    label: t('About'),
+    items: [
+      {
+        label: t('Documentation'),
+        href: 'https://docs.pancakeswap.finance/contact-us',
+        isHighlighted: true,
+      },
+      {
+        label: t('Magik Finance'),
+        href: 'https://magik.finance',
+      },
+      {
+        label: t('MagikFarm'),
+        href: 'https://medium.com/pancakeswap',
+      },
+      
+      {
+        label: '—',
+      },
+    ],
+  },    
+
+          
+      /*               
+  {
+    label: t('Help'),
+    items: [
+      {
+        label: t('Customer Support'),
+        href: 'https://docs.pancakeswap.finance/contact-us/customer-support',
+      },
+      {
+        label: t('Troubleshooting'),
+        href: 'https://docs.pancakeswap.finance/help/troubleshooting',
+      },
+      {
+        label: t('Guides'),
+        href: 'https://docs.pancakeswap.finance/get-started',
+      },
+    ],
+  }, 
+  {
+    label: t('Developers'),
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/pancakeswap',
+      },
+      {
+        label: t('Documentation'),
+        href: 'https://docs.pancakeswap.finance',
+      },
+      {
+        label: t('Bug Bounty'),
+        href: 'https://docs.pancakeswap.finance/code/bug-bounty',
+      },
+      {
+        label: t('Audits'),
+        href: 'https://docs.pancakeswap.finance/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited',
+      },
+      {
+        label: t('Careers'),
+        href: 'https://docs.pancakeswap.finance/hiring/become-a-chef',
+      },
+    ],
+  },
+  
+      */      
+]
